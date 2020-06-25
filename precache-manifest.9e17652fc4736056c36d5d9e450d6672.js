ab02a59c4dcbd35cd89e36b212dd0b83b0a2bb78
ThinkTink <ThinkTinkEmail@protonmail.com>
2020-06-24 18:54:44 -0700

    Deploy

---
+++ precache-manifest.9e17652fc4736056c36d5d9e450d6672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37510139283027f39d46d2f893e41a6c",
+    "url": "/NASAPhotoAPI/index.html"
+  },
+  {
+    "revision": "8cbab68db9eb0791a49f",
+    "url": "/NASAPhotoAPI/static/css/main.5c9fac5a.chunk.css"
+  },
+  {
+    "revision": "6ec1e9ce352d97b0862e",
+    "url": "/NASAPhotoAPI/static/js/2.04c12c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASAPhotoAPI/static/js/2.04c12c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cbab68db9eb0791a49f",
+    "url": "/NASAPhotoAPI/static/js/main.3b01d2e6.chunk.js"
+  },
+  {
+    "revision": "feff9fb4d487ad92a069",
+    "url": "/NASAPhotoAPI/static/js/runtime-main.4ba17563.js"
+  }
+]);
